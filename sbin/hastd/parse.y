kelvinyu/unix-history-repo

commit 2a4dc188787c12c650912575604a415e94f528fc

Allow to checksum on-the-wire data using either CRC32 or SHA256.
MFC after:	2 weeks


git-svn-id: svn+ssh://svn.freebsd.org/base/head@219351 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sbin/hastd/parse.y
@@ -1,6 +1,7 @@
 %{
 /*-
  * Copyright (c) 2009-2010 The FreeBSD Foundation
+ * Copyright (c) 2011 Pawel Jakub Dawidek <pawel@dawidek.net>
  * All rights reserved.
  *
  * This software was developed by Pawel Jakub Dawidek under sponsorship from
@@ -60,6 +61,7 @@ static bool mynode, hadmynode;
 static char depth0_control[HAST_ADDRSIZE];
 static char depth0_listen[HAST_ADDRSIZE];
 static int depth0_replication;
+static int depth0_checksum;
 static int depth0_timeout;
 static char depth0_exec[PATH_MAX];
 
@@ -167,6 +169,7 @@ yy_config_parse(const char *config, bool exitonerror)
 
 	depth0_timeout = HAST_TIMEOUT;
 	depth0_replication = HAST_REPLICATION_MEMSYNC;
+	depth0_checksum = HAST_CHECKSUM_NONE;
 	strlcpy(depth0_control, HAST_CONTROL, sizeof(depth0_control));
 	strlcpy(depth0_listen, HASTD_LISTEN, sizeof(depth0_listen));
 	depth0_exec[0] = '\0';
@@ -223,6 +226,13 @@ yy_config_parse(const char *config, bool exitonerror)
 			 */
 			curres->hr_replication = depth0_replication;
 		}
+		if (curres->hr_checksum == -1) {
+			/*
+			 * Checksum is not set at resource-level.
+			 * Use global or default setting.
+			 */
+			curres->hr_checksum = depth0_checksum;
+		}
 		if (curres->hr_timeout == -1) {
 			/*
 			 * Timeout is not set at resource-level.
@@ -256,11 +266,13 @@ yy_config_free(struct hastd_config *config)
 }
 %}
 
-%token CONTROL LISTEN PORT REPLICATION TIMEOUT EXEC EXTENTSIZE RESOURCE NAME LOCAL REMOTE ON
-%token FULLSYNC MEMSYNC ASYNC
+%token CONTROL LISTEN PORT REPLICATION CHECKSUM
+%token TIMEOUT EXEC EXTENTSIZE RESOURCE NAME LOCAL REMOTE ON
+%token FULLSYNC MEMSYNC ASYNC NONE CRC32 SHA256
 %token NUM STR OB CB
 
 %type <num> replication_type
+%type <num> checksum_type
 
 %union
 {
@@ -284,6 +296,8 @@ statement:
 	listen_statement
 	|
 	replication_statement
+	|
+	checksum_statement
 	|
 	timeout_statement
 	|
@@ -376,6 +390,30 @@ replication_type:
 	MEMSYNC		{ $$ = HAST_REPLICATION_MEMSYNC; }
 	|
 	ASYNC		{ $$ = HAST_REPLICATION_ASYNC; }
+	;
+
+checksum_statement:	CHECKSUM checksum_type
+	{
+		switch (depth) {
+		case 0:
+			depth0_checksum = $2;
+			break;
+		case 1:
+			if (curres != NULL)
+				curres->hr_checksum = $2;
+			break;
+		default:
+			assert(!"checksum at wrong depth level");
+		}
+	}
+	;
+
+checksum_type:
+	NONE		{ $$ = HAST_CHECKSUM_NONE; }
+	|
+	CRC32		{ $$ = HAST_CHECKSUM_CRC32; }
+	|
+	SHA256		{ $$ = HAST_CHECKSUM_SHA256; }
 	;
 
 timeout_statement:	TIMEOUT NUM
@@ -570,6 +608,7 @@ resource_start:	STR
 		curres->hr_role = HAST_ROLE_INIT;
 		curres->hr_previous_role = HAST_ROLE_INIT;
 		curres->hr_replication = -1;
+		curres->hr_checksum = -1;
 		curres->hr_timeout = -1;
 		curres->hr_exec[0] = '\0';
 		curres->hr_provname[0] = '\0';
@@ -587,6 +626,8 @@ resource_entries:
 
 resource_entry:
 	replication_statement
+	|
+	checksum_statement
 	|
 	timeout_statement
 	|

sbin/hastctl/Makefile
@@ -6,8 +6,9 @@
 
 PROG=	hastctl
 SRCS=	activemap.c
+SRCS+=	crc32.c
 SRCS+=	ebuf.c
-SRCS+=	hast_proto.c hastctl.c
+SRCS+=	hast_checksum.c hast_proto.c hastctl.c
 SRCS+=	metadata.c
 SRCS+=	nv.c
 SRCS+=	parse.y pjdlog.c

sbin/hastd/Makefile
@@ -4,9 +4,9 @@
 
 PROG=	hastd
 SRCS=	activemap.c
-SRCS+=	control.c
+SRCS+=	control.c crc32.c
 SRCS+=	ebuf.c event.c
-SRCS+=	hast_proto.c hastd.c hooks.c
+SRCS+=	hast_checksum.c hast_proto.c hastd.c hooks.c
 SRCS+=	metadata.c
 SRCS+=	nv.c
 SRCS+=	secondary.c

sbin/hastd/control.c
@@ -43,6 +43,7 @@ __FBSDID("$FreeBSD$");
 
 #include "hast.h"
 #include "hastd.h"
+#include "hast_checksum.h"
 #include "hast_proto.h"
 #include "hooks.h"
 #include "nv.h"
@@ -246,6 +247,8 @@ control_status(struct hastd_config *cfg, struct nv *nvout,
 		nv_add_string(nvout, "unknown", "replication%u", no);
 		break;
 	}
+	nv_add_string(nvout, checksum_name(res->hr_checksum),
+	    "checksum%u", no);
 	nv_add_string(nvout, role2str(res->hr_role), "role%u", no);
 
 	switch (res->hr_role) {

sbin/hastd/crc32.c
@@ -1,0 +1,115 @@
+/*-
+ *  COPYRIGHT (C) 1986 Gary S. Brown.  You may use this program, or
+ *  code or tables extracted from it, as desired without restriction.
+ */
+
+/*
+ *  First, the polynomial itself and its table of feedback terms.  The
+ *  polynomial is
+ *  X^32+X^26+X^23+X^22+X^16+X^12+X^11+X^10+X^8+X^7+X^5+X^4+X^2+X^1+X^0
+ *
+ *  Note that we take it "backwards" and put the highest-order term in
+ *  the lowest-order bit.  The X^32 term is "implied"; the LSB is the
+ *  X^31 term, etc.  The X^0 term (usually shown as "+1") results in
+ *  the MSB being 1
+ *
+ *  Note that the usual hardware shift register implementation, which
+ *  is what we're using (we're merely optimizing it by doing eight-bit
+ *  chunks at a time) shifts bits into the lowest-order term.  In our
+ *  implementation, that means shifting towards the right.  Why do we
+ *  do it this way?  Because the calculated CRC must be transmitted in
+ *  order from highest-order term to lowest-order term.  UARTs transmit
+ *  characters in order from LSB to MSB.  By storing the CRC this way
+ *  we hand it to the UART in the order low-byte to high-byte; the UART
+ *  sends each low-bit to hight-bit; and the result is transmission bit
+ *  by bit from highest- to lowest-order term without requiring any bit
+ *  shuffling on our part.  Reception works similarly
+ *
+ *  The feedback terms table consists of 256, 32-bit entries.  Notes
+ *
+ *      The table can be generated at runtime if desired; code to do so
+ *      is shown later.  It might not be obvious, but the feedback
+ *      terms simply represent the results of eight shift/xor opera
+ *      tions for all combinations of data and CRC register values
+ *
+ *      The values must be right-shifted by eight bits by the "updcrc
+ *      logic; the shift must be unsigned (bring in zeroes).  On some
+ *      hardware you could probably optimize the shift in assembler by
+ *      using byte-swap instructions
+ *      polynomial $edb88320
+ *
+ *
+ * CRC32 code derived from work by Gary S. Brown.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <stdint.h>
+
+#include <crc32.h>
+
+uint32_t crc32_tab[] = {
+	0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
+	0xe963a535, 0x9e6495a3,	0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
+	0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2,
+	0xf3b97148, 0x84be41de,	0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7,
+	0x136c9856, 0x646ba8c0, 0xfd62f97a, 0x8a65c9ec,	0x14015c4f, 0x63066cd9,
+	0xfa0f3d63, 0x8d080df5,	0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172,
+	0x3c03e4d1, 0x4b04d447, 0xd20d85fd, 0xa50ab56b,	0x35b5a8fa, 0x42b2986c,
+	0xdbbbc9d6, 0xacbcf940,	0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59,
+	0x26d930ac, 0x51de003a, 0xc8d75180, 0xbfd06116, 0x21b4f4b5, 0x56b3c423,
+	0xcfba9599, 0xb8bda50f, 0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924,
+	0x2f6f7c87, 0x58684c11, 0xc1611dab, 0xb6662d3d,	0x76dc4190, 0x01db7106,
+	0x98d220bc, 0xefd5102a, 0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433,
+	0x7807c9a2, 0x0f00f934, 0x9609a88e, 0xe10e9818, 0x7f6a0dbb, 0x086d3d2d,
+	0x91646c97, 0xe6635c01, 0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e,
+	0x6c0695ed, 0x1b01a57b, 0x8208f4c1, 0xf50fc457, 0x65b0d9c6, 0x12b7e950,
+	0x8bbeb8ea, 0xfcb9887c, 0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65,
+	0x4db26158, 0x3ab551ce, 0xa3bc0074, 0xd4bb30e2, 0x4adfa541, 0x3dd895d7,
+	0xa4d1c46d, 0xd3d6f4fb, 0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0,
+	0x44042d73, 0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9, 0x5005713c, 0x270241aa,
+	0xbe0b1010, 0xc90c2086, 0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f,
+	0x5edef90e, 0x29d9c998, 0xb0d09822, 0xc7d7a8b4, 0x59b33d17, 0x2eb40d81,
+	0xb7bd5c3b, 0xc0ba6cad, 0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a,
+	0xead54739, 0x9dd277af, 0x04db2615, 0x73dc1683, 0xe3630b12, 0x94643b84,
+	0x0d6d6a3e, 0x7a6a5aa8, 0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1,
+	0xf00f9344, 0x8708a3d2, 0x1e01f268, 0x6906c2fe, 0xf762575d, 0x806567cb,
+	0x196c3671, 0x6e6b06e7, 0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc,
+	0xf9b9df6f, 0x8ebeeff9, 0x17b7be43, 0x60b08ed5, 0xd6d6a3e8, 0xa1d1937e,
+	0x38d8c2c4, 0x4fdff252, 0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b,
+	0xd80d2bda, 0xaf0a1b4c, 0x36034af6, 0x41047a60, 0xdf60efc3, 0xa867df55,
+	0x316e8eef, 0x4669be79, 0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236,
+	0xcc0c7795, 0xbb0b4703, 0x220216b9, 0x5505262f, 0xc5ba3bbe, 0xb2bd0b28,
+	0x2bb45a92, 0x5cb36a04, 0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d,
+	0x9b64c2b0, 0xec63f226, 0x756aa39c, 0x026d930a, 0x9c0906a9, 0xeb0e363f,
+	0x72076785, 0x05005713, 0x95bf4a82, 0xe2b87a14, 0x7bb12bae, 0x0cb61b38,
+	0x92d28e9b, 0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21, 0x86d3d2d4, 0xf1d4e242,
+	0x68ddb3f8, 0x1fda836e, 0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777,
+	0x88085ae6, 0xff0f6a70, 0x66063bca, 0x11010b5c, 0x8f659eff, 0xf862ae69,
+	0x616bffd3, 0x166ccf45, 0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2,
+	0xa7672661, 0xd06016f7, 0x4969474d, 0x3e6e77db, 0xaed16a4a, 0xd9d65adc,
+	0x40df0b66, 0x37d83bf0, 0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9,
+	0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6, 0xbad03605, 0xcdd70693,
+	0x54de5729, 0x23d967bf, 0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
+	0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
+};
+
+/*
+ * A function that calculates the CRC-32 based on the table above is
+ * given below for documentation purposes. An equivalent implementation
+ * of this function that's actually used in the kernel can be found
+ * in sys/libkern.h, where it can be inlined.
+ *
+ *	uint32_t
+ *	crc32(const void *buf, size_t size)
+ *	{
+ *		const uint8_t *p = buf;
+ *		uint32_t crc;
+ *
+ *		crc = ~0U;
+ *		while (size--)
+ *			crc = crc32_tab[(crc ^ *p++) & 0xFF] ^ (crc >> 8);
+ *		return crc ^ ~0U;
+ *	}
+ */

sbin/hastd/crc32.h
@@ -1,0 +1,28 @@
+/*-
+ *  COPYRIGHT (C) 1986 Gary S. Brown.  You may use this program, or
+ *  code or tables extracted from it, as desired without restriction.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _CRC32_H_
+#define	_CRC32_H_
+
+#include <stdint.h>	/* uint32_t */
+#include <stdlib.h>	/* size_t */
+
+extern uint32_t crc32_tab[];
+
+static __inline uint32_t
+crc32(const void *buf, size_t size)
+{
+	const uint8_t *p = buf;
+	uint32_t crc;
+
+	crc = ~0U;
+	while (size--)
+		crc = crc32_tab[(crc ^ *p++) & 0xFF] ^ (crc >> 8);
+	return (crc ^ ~0U);
+}
+
+#endif	/* !_CRC32_H_ */

sbin/hastd/hast.conf.5
@@ -1,5 +1,5 @@
 .\" Copyright (c) 2010 The FreeBSD Foundation
-.\" Copyright (c) 2010 Pawel Jakub Dawidek <pjd@FreeBSD.org>
+.\" Copyright (c) 2010-2011 Pawel Jakub Dawidek <pawel@dawidek.net>
 .\" All rights reserved.
 .\"
 .\" This software was developed by Pawel Jakub Dawidek under sponsorship from
@@ -59,6 +59,7 @@ file is following:
 control <addr>
 listen <addr>
 replication <mode>
+checksum <algorithm>
 timeout <seconds>
 exec <path>
 
@@ -77,6 +78,7 @@ on <node> {
 resource <name> {
 	# Resource section
 	replication <mode>
+	checksum <algorithm>
 	name <name>
 	local <path>
 	timeout <seconds>
@@ -200,6 +202,18 @@ latency is too high for other modes.
 The
 .Ic async
 replication mode is currently not implemented.
+.El
+.It Ic checksum Aq algorithm
+.Pp
+Checksum algorithm should be one of the following:
+.Bl -tag -width ".Ic sha256"
+.It Ic none
+No checksum will be calculated for the data being send over the network.
+This is the default setting.
+.It Ic crc32
+CRC32 checksum will be calculated.
+.It Ic sha256
+SHA256 checksum will be calculated.
 .El
 .It Ic timeout Aq seconds
 .Pp

sbin/hastd/hast.h
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2009-2010 The FreeBSD Foundation
+ * Copyright (c) 2011 Pawel Jakub Dawidek <pawel@dawidek.net>
  * All rights reserved.
  *
  * This software was developed by Pawel Jakub Dawidek under sponsorship from
@@ -116,6 +117,10 @@ struct hastd_config {
 #define	HAST_REPLICATION_MEMSYNC	1
 #define	HAST_REPLICATION_ASYNC		2
 
+#define	HAST_CHECKSUM_NONE	0
+#define	HAST_CHECKSUM_CRC32	1
+#define	HAST_CHECKSUM_SHA256	2
+
 /*
  * Structure that describes single resource.
  */
@@ -132,6 +137,8 @@ struct hast_resource {
 	int	hr_keepdirty;
 	/* Path to a program to execute on various events. */
 	char	hr_exec[PATH_MAX];
+	/* Checksum algorithm. */
+	int	hr_checksum;
 
 	/* Path to local component. */
 	char	hr_localpath[PATH_MAX];

commit 0266675746dad72c8a079a8969d31758a5da6ff8

Allow to specify remote as 'none' again which was broken by r219351, where 'none' was defined as a value for checksum.
Reported by:	trasz
MFC after:	1 week

sbin/hastd/parse.y
@@ -289,6 +289,7 @@ yy_config_free(struct hastd_config *config)
 %token FULLSYNC MEMSYNC ASYNC NONE CRC32 SHA256 HOLE LZF
 %token NUM STR OB CB
 
+%type <str> remote_str
 %type <num> replication_type
 %type <num> checksum_type
 %type <num> compression_type
@@ -796,7 +797,7 @@ resource_node_entry:
 	source_statement
 	;
 
-remote_statement:	REMOTE STR
+remote_statement:	REMOTE remote_str
 	{
 		assert(depth == 2);
 		if (mynode) {
@@ -811,6 +812,12 @@ remote_statement:	REMOTE STR
 		}
 		free($2);
 	}
+	;
+
+remote_str:
+	NONE		{ $$ = strdup("none"); }
+	|
+	STR		{ }
 	;
 
 source_statement:	SOURCE STR